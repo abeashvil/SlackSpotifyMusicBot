6306a0af88e25729f0b6abe62c83bd3662e05099
abeashvil <abrahama9796@halsey157.com>
2022-04-06 21:07:59 -0400

    added some clarifying comments

--- src/main.py
+++ src/main.py
@@ -8,28 +8,20 @@ from dotenv import load_dotenv
 from slack_sdk import WebClient
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
+#code for setting up the bots with the tokens and also the scopes needed for reading recently played songs along with making a playlist
+#also sets up the slack bot client using the slack bot token found in my .env However you cant see my .env because I am not charlie
 load_dotenv() 
 scope = "user-read-recently-played"
 scope2 = "playlist-modify-public";
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 slack_client = WebClient(SLACK_BOT_TOKEN)
 
-# recent_msg = slack_client.conversations_history(channel ='C02R31HJ33Q', limit= 1)
-# data_mes = recent_msg.data['messages'][0]['text']
-# while data_mes != 'stop' and data_mes != 'Stop':
-#     time.sleep(10)
-#     recent_msg = slack_client.conversations_history(channel ='C02R31HJ33Q', limit= 1)
-#     data_mes = recent_msg.data['messages'][0]['text']
-#     print(data_mes)
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 most_recent_song = sp.current_user_recently_played(1)
-
-
-most_recent_song_uri = most_recent_post['items'][0]['track']['uri']
-most_recent_song_link = most_recent_post['items'][0]['track']['external_urls']['spotify']
-
-previous_song_link
+previous_song_link = most_recent_song['items'][0]['track']['external_urls']['spotify']
 
 while True:
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
@@ -38,18 +30,19 @@ while True:
     most_recent_song = sp.current_user_recently_played(1)
 
 
-    most_recent_song_uri = most_recent_post['items'][0]['track']['uri']
-    most_recent_song_link = most_recent_post['items'][0]['track']['external_urls']['spotify']
+    most_recent_song_uri = most_recent_song['items'][0]['track']['uri']
+    most_recent_song_link = most_recent_song['items'][0]['track']['external_urls']['spotify']
 
+    
+    if most_recent_song_link != previous_song_link:
+        slack_client.chat_postMessage(channel='C02R31HJ33Q', text = most_recent_song_link)
+        
+        message = slack_client.conversations_history(channel ='C02R31HJ33Q', limit= 1)
 
-    slack_client.chat_postMessage(channel='C02R31HJ33Q', text = most_recent_song_link)
+        data_mes = message.data
+        ts = data_mes['messages'][0]['ts']
+        slack_client.reactions_add(channel='C02R31HJ33Q', name = 'heart', timestamp=ts)
 
+        previous_song_link = most_recent_song_link
 
-    message = slack_client.conversations_history(channel ='C02R31HJ33Q', limit= 1)
-
-
-    data_mes = message.data
-    ts = data_mes['messages'][0]['ts']
-    slack_client.reactions_add(channel='C02R31HJ33Q', name = 'heart', timestamp=ts)
-    # slack_client.reactions_add()
 
