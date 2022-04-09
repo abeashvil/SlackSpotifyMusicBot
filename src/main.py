4cf818fb4d6bf32c5ca0c726287ba737a4649de8
abeashvil <abrahama9796@halsey157.com>
2022-04-09 02:02:42 -0400

    fix channel creation/ checking if already created

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from logging.handlers import SYSLOG_UDP_PORT
+from calendar import c
 import spotipy
 import os
 import time
@@ -27,44 +27,44 @@ def send_new_song(channelid):
     most_recent_song_uri = most_recent_song['items'][0]['track']['uri']
     most_recent_song_link = most_recent_song['items'][0]['track']['external_urls']['spotify']
 
-    previous_song = slack_client.conversations_history(channel ='channelid', limit= 1).data['messages'][0]['text'][1:-1]
+    previous_song = slack_client.conversations_history(channel =channelid, limit= 1).data['messages'][0]['text'][1:-1]
 
     print(previous_song)
 
     
     if most_recent_song_link != previous_song:       
-        slack_client.chat_postMessage(channel='channelid', text = most_recent_song_link)
+        slack_client.chat_postMessage(channel=channelid, text = most_recent_song_link)
      
-        message = slack_client.conversations_history(channel ='C02R31HJ33Q', limit= 1)
+        message = slack_client.conversations_history(channel =channelid, limit= 1)
 
         data_mes = message.data
         ts = data_mes['messages'][0]['ts']
-        slack_client.reactions_add(channel='channelid', name = 'heart', timestamp=ts)
+        slack_client.reactions_add(channel=channelid, name = 'heart', timestamp=ts)
     
-
-
-def main():
-    channel = ''
+def setup():
+    channel = dict()
     try:
-        channel = slack_client.channels_create("song bot")
-    except:
+        channel = slack_client.conversations_create(name = "songbot", is_private= False)
+    except Exception:
         print("failed, channel already exists")
         # creates a list of all the channels in the slack conversation
         channelList = slack_client.conversations_list()['channels']
 
-        print(channelList)
         #if the channel already exists, we need to set the channel variable to it and also have the bot join the channel
         for i in range(len(channelList)):
-            print(channelList[i]['name'])
-            if(channelList[i]["name"] == "song bot"):
+            if(channelList[i]["name"] == "songbot"):
                 channel = channelList[i]
                 slack_client.conversations_join(channel = channel['id'])
 
-    print(channel)
+    channelId = channel['id']
 
-    # send_new_song(channelId)
+    return channelId
 
 
+def main():
+    id = setup()
+
+    send_new_song(id)
 
 
 if __name__ == "__main__":
